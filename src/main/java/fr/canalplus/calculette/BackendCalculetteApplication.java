ee0bfcfe1572efb46736c6a08c17bfce59bdf6d4
nabil.moalla <nabilmoalla@hotmail.com>
2023-03-15 23:55:18 +0100

    first commit

---
+++ src/main/java/fr/canalplus/calculette/BackendCalculetteApplication.java
@@ -0,0 +1,13 @@
+package fr.canalplus.calculette.backendcalculette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCalculetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCalculetteApplication.class, args);
+	}
+
+}
